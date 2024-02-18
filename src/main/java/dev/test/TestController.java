404cf55fac8a013637a627884e05a9d31ebdc5bf
kypdk@proton.me <kypdk@proton.me>
2024-02-19 02:09:18 +0300

    hatalar

--- src/main/java/dev/test/TestController.java
+++ src/main/java/dev/test/TestController.java
@@ -1,5 +1,6 @@
 package dev.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @Autowired
@@ -17,6 +19,26 @@ public class TestController {
     @GetMapping("/yap")
     public ResponseEntity<String> calistir() {
         
+        return ResponseEntity.ok(testService.test());
+    }
+
+    @GetMapping("/hata")
+    public ResponseEntity<String> hata() {
+
+        try {
+            throw new Exception("Bu bir örnek istisna");
+        } catch (Exception e) {
+            log.error("İstisna yakalandı: " + e);
+        }
+
+        return ResponseEntity.ok(testService.test());
+    }
+
+    @GetMapping("/hata_iki")
+    public ResponseEntity<String> hata_iki() {
+
+        int a = 5 / 0;
+
         return ResponseEntity.ok(testService.test());
     }
 }
